a2fddf0521f5c8df175b1f3fe859c71fa564051b
kkhwjnrk <kukuhwijanarko07@gmail.com>
2023-03-08 13:24:15 +0700

    update webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -69,6 +69,9 @@ module.exports = {
         },
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+    })
   ],
 };
