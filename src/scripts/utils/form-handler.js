10a47cb15005bfbd0bc7eeedbc8cdc48f5801ddc
kkhwjnrk <kukuhwijanarko07@gmail.com>
2023-03-09 09:35:23 +0700

    initial

--- src/scripts/utils/form-handler.js
+++ src/scripts/utils/form-handler.js
@@ -19,12 +19,11 @@ const FormHandler = {
       id: this._restaurantId,
       name: this._nameInput.value,
       review: this._reviewInput.value,
-      reviewDate: new Date().toLocaleDateString('id-ID', { year: 'numeric', month: 'long', day: 'numeric' }),
+      // reviewDate: new Date().toLocaleDateString('id-ID', { year: 'numeric', month: 'long', day: 'numeric' }),
     };
 
     try {
       await RestaurantSource.postReview(review);
-      console.log(review.date);
       this._addReviewToContainer(review);
       this._resetForm();
     } catch (error) {
@@ -36,10 +35,10 @@ const FormHandler = {
     const reviewTemplate = `
       <article class="review-data">
         <p>${review.name}</p>
-        <p>${review.reviewDate}</p>
         <p><em>"${review.review}"</em></p>
         </article>
         `;
+    // <p>${review.reviewDate}</p>
     this._reviewsContainer.insertAdjacentHTML('beforeend', reviewTemplate);
   },
 
